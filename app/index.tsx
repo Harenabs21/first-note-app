5bbcf5f539da039dfe8ca03d5cb4fcf64849e7cd
Harenabs21 <hei.harena.6@gmail.com>
2024-12-12 16:26:31 +0300

    dep: add nativewind dependency and delete all unused component

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "react-native";
+import "../global.css"
+
+export default function App() {
+    return(
+      <Text className="text-red-700 text-3xl bg-black mt-10"> 
+        Hello world 
+      </Text>
+    )
+}
